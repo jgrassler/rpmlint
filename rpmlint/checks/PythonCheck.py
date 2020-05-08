a2b13ec3e9a8b3ce59e018040ca5fee06e39132e
Johannes Grassler <johannes.grassler@suse.com>
2020-05-08 16:43:20 +0200

    PythonCheck: add tests (openSUSE only for now)
    This commit adds tests for PythonCheck. For now they are
    openSUSE only since it turns out that requirements-parser is
    not available on Fedora.

--- rpmlint/checks/PythonCheck.py
+++ rpmlint/checks/PythonCheck.py
@@ -1,151 +1,145 @@
 import os
 import re
-import requirements
 
+import requirements
 from rpmlint.checks.AbstractCheck import AbstractFilesCheck
 
 # Warning messages
-WARNS = {
-    "tests": "python-tests-in-package",
-    "doc": "python-doc-in-package",
-    "req-missing": "python-egginfo-require-not-in-spec",
-    "src": "python-src-in-package"
-  }
+WARNS = {'tests': 'python-tests-in-package',
+         'doc': 'python-doc-in-package',
+         'req-missing': 'python-egginfo-require-not-in-spec',
+         'src': 'python-src-in-package'}
 
 # Error messages
-ERRS = {
-    "egg-distutils": "python-egg-info-distutils-style",
-    "tests": "python-tests-in-site-packages",
-    "doc": "python-doc-in-site-packages",
-    "src": "python-src-in-site-packages"
-  }
+ERRS = {'egg-distutils': 'python-egg-info-distutils-style',
+        'tests': 'python-tests-in-site-packages',
+        'doc': 'python-doc-in-site-packages',
+        'src': 'python-src-in-site-packages'}
 
 # Paths that shouldn't be in any packages, ever, because they clobber global
 # name space.
-ERR_PATHS = {
-    "/usr/lib[^/]*/python[^/]*/site-packages/tests?$": ERRS["tests"],
-    "/usr/lib[^/]*/python[^/]*/site-packages/docs?$": ERRS["doc"],
-    "/usr/lib[^/]*/python[^/]*/site-packages/src$": ERRS["src"]
-  }
+ERR_PATHS = {'/usr/lib[^/]*/python[^/]*/site-packages/tests?$': ERRS['tests'],
+             '/usr/lib[^/]*/python[^/]*/site-packages/docs?$': ERRS['doc'],
+             '/usr/lib[^/]*/python[^/]*/site-packages/src$': ERRS['src']}
 
 # Paths that shouldn't be in any packages, but might need to be under
 # sufficiently special circumstances.
-WARN_PATHS = {
-    "/usr/lib[^/]*/python[^/]*/site-packages/[^/]+/tests?$": WARNS["tests"],
-    "/usr/lib[^/]*/python[^/]*/site-packages/[^/]+/docs?$": WARNS["doc"],
-    "/usr/lib[^/]*/python[^/]*/site-packages/[^/]+/src$": WARNS["src"]
-  }
+WARN_PATHS = {'/usr/lib[^/]*/python[^/]*/site-packages/[^/]+/tests?$': WARNS['tests'],
+              '/usr/lib[^/]*/python[^/]*/site-packages/[^/]+/docs?$': WARNS['doc'],
+              '/usr/lib[^/]*/python[^/]*/site-packages/[^/]+/src$': WARNS['src']}
+
 
 class PythonCheck(AbstractFilesCheck):
     def __init__(self, config, output):
         super().__init__(config, output, r'.*')
 
     def check_file(self, pkg, filename):
-      egg_info_re = re.compile('.*egg-info$')
+        egg_info_re = re.compile('.*egg-info$')
 
-      if egg_info_re.match(filename):
-        self.check_egginfo(pkg, filename)
+        if egg_info_re.match(filename):
+            self.check_egginfo(pkg, filename)
 
-      for path in WARN_PATHS:
-        path_re = re.compile(path)
+        for path in WARN_PATHS:
+            path_re = re.compile(path)
 
-        if path_re.match(filename):
-          self.output.add_info("W", pkg, WARN_PATHS[path], filename)
+            if path_re.match(filename):
+                self.output.add_info('W', pkg, WARN_PATHS[path], filename)
 
-      for path in ERR_PATHS:
-        path_re = re.compile(path)
+        for path in ERR_PATHS:
+            path_re = re.compile(path)
 
-        if path_re.match(filename):
-          self.output.add_info("E", pkg, ERR_PATHS[path], filename)
+            if path_re.match(filename):
+                self.output.add_info('E', pkg, ERR_PATHS[path], filename)
 
     def check_egginfo(self, pkg, filename):
-      """
-      Check type of egg-info metadata and check Requires against egg-info
-      metadata if applicable.
-      """
-      # Check for (deprecated) distutils style metadata.
-      if os.path.isfile(pkg.dirName() + filename):
-        self.output.add_info("E", pkg, ERRS["egg-distutils"], filename)
-        # No need to proceed any further here since distutils style metadata
-        # doesn't have requirements information.
-        return
+        """
+        Check type of egg-info metadata and check Requires against egg-info
+        metadata if applicable.
+        """
+        # Check for (deprecated) distutils style metadata.
+        if os.path.isfile(os.path.join(pkg.dirName(), filename.lstrip('/'))):
+            self.output.add_info('E', pkg, ERRS['egg-distutils'], filename)
+            # No need to proceed any further here since distutils style metadata
+            # doesn't have requirements information.
+            return
 
-      # egg-info is a directory, check requires.txt against spec's Requires.
-      # We will ignore packages that do not have a requires.txt here.
-      requires_path = pkg.dirName() + filename + "/" + "requires.txt"
-      if os.path.isfile(requires_path):
-        self.compare_requires(pkg, requires_path)
+        # egg-info is a directory, check requires.txt against spec's Requires.
+        # We will ignore packages that do not have a requires.txt here.
+        requires_path = os.path.join(pkg.dirName(), filename.lstrip('/'), 'requires.txt')
+        relative_requires_path = os.path.join(filename, 'requires.txt')
+        if os.path.isfile(requires_path):
+            self.compare_requires(pkg, requires_path, relative_requires_path)
 
-    def compare_requires(self, pkg, requires_path):
-      """
-      Check whether each egg name from requirements.txt occurs in the spec's
-      Requirements somewhere and output a warning if it's nowhere to be found.
-      """
-      from_egg = self.egg_requires(requires_path)
-      from_spec = self.package_requires(pkg)
+    def compare_requires(self, pkg, requires_path, relative_requires_path):
+        """
+        Check whether each egg name from requirements.txt occurs in the spec's
+        Requirements somewhere and output a warning if it's nowhere to be found.
+        """
+        from_egg = self.egg_requires(requires_path)
+        from_spec = self.package_requires(pkg)
 
-      for egg_require in from_egg:
-        found = False
-        for spec_require in from_spec:
+        for egg_require in from_egg:
+            found = False
 
-          if egg_require in spec_require:
-            found = True
+            for spec_require in from_spec:
+                if egg_require in spec_require:
+                    found = True
 
-        if not found:
-          self.output.add_info("W", pkg, WARNS["req-missing"], requires_path, ":", egg_require)
+            if not found:
+                self.output.add_info('W', pkg, WARNS['req-missing'], relative_requires_path, egg_require)
 
     def egg_requires(self, requires_path):
-      """
-      Generate a flat list of unversioned, lowercase require names from an
-      egg-info directory's requires.txt.
-      """
+        """
+        Generate a flat list of unversioned, lowercase require names from an
+        egg-info directory's requires.txt.
+        """
 
-      try:
-        f = open(requires_path, 'r')
-      except Exception:
-        # Some .egg-info directories do not have a requires.txt, so this may
-        # fail.
-        return
+        try:
+            f = open(requires_path, 'r')
+        except Exception:
+            # Some .egg-info directories do not have a requires.txt, so this may
+            # fail.
+            return
 
-      lines = ""
-      requires = []
+        lines = ''
+        requires = []
 
-      for line in f.readlines():
-        # FIXME: this code ignores conditional sections, such as
-        #
-        #   [:python_version < '3']
-        #
-        # It does this because requirements-parser cannot deal with these
-        # sections, hence we just filter out the section header. The best
-        # approach for fixing this would be submitting patch against
-        # requirements-parser that adds the capability to deal with sections by
-        # adding such conditionals to every require in that section as
-        # environment markers. Once such a patch exists, we can filter such
-        # markers here on a per-requirement basis.
+        for line in f.readlines():
+            # FIXME: this code ignores conditional sections, such as
+            #
+            #   [:python_version < '3']
+            #
+            # It does this because requirements-parser cannot deal with these
+            # sections, hence we just filter out the section header. The best
+            # approach for fixing this would be submitting patch against
+            # requirements-parser that adds the capability to deal with sections by
+            # adding such conditionals to every require in that section as
+            # environment markers. Once such a patch exists, we can filter such
+            # markers here on a per-requirement basis.
 
-        if line.startswith("["):
-          continue
-        lines += line
+            if line.startswith('['):
+                continue
+            lines += line
 
-      requires_raw = requirements.parser.parse(lines)
+        requires_raw = requirements.parser.parse(lines)
 
-      for r in requires_raw:
-        requires.append(r.name.lower())
+        for r in requires_raw:
+            requires.append(r.name.lower())
 
-      f.close()
+        f.close()
 
-      return requires
+        return requires
 
     def package_requires(self, pkg):
-      """
-      Generate a flat list of unversioned, lowercase requirement names from the
-      package's, spec.
-      """
+        """
+        Generate a flat list of unversioned, lowercase requirement names from the
+        package's, spec.
+        """
 
-      requires_raw = pkg.requires
-      requires = []
+        requires_raw = pkg.requires
+        requires = []
 
-      for r in requires_raw:
-        requires.append(r[0].lower())
+        for r in requires_raw:
+            requires.append(r[0].lower())
 
-      return requires
+        return requires
